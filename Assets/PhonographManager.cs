19c3e52697ea73cc9798cd2f1fa9b2ab85f7c61b
Koen H <49477924+Koen-H@users.noreply.github.com>
2023-04-06 13:34:27 +0200

    Phonograph!

---
+++ Assets/PhonographManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PhonographManager : NetworkBehaviour
+{
+    [SerializeField] List<VynilPlate> vinylPlates;
+    private Dictionary<VynilPlateName, VynilPlate> vinylPlatesDic = new();
+    [SerializeField] XRLockSocketInteractor socket;
+    AudioSource audioSource;
+
+    public void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        foreach (VynilPlate plate in vinylPlates)
+        {
+            vinylPlatesDic.Add(plate.vinylPlateName,plate);
+        }
+    }
+
+    [ClientRpc]
+    public void PlayerSongClientRpc(int platenameInt)
+    {
+        PlaySong(platenameInt);
+    }
+    [ClientRpc]
+    public void StopSongClientRpc()
+    {
+        StopSong();
+    }
+
+    public void OnSongEntered()
+    {
+        VynilPlateName plateName = socket.GetOldestInteractableSelected().transform.GetComponent<VinylPlateManager>().plateName;
+        int plateNameInt = (int)plateName;
+        PlaySong(plateNameInt);
+        PlayerSongClientRpc(plateNameInt);
+    }
+    
+    public void OnSongLeaving()
+    {
+        StopSong();
+        StopSongClientRpc();
+    }
+
+
+    public void PlaySong(int platenameInt)
+    {
+        audioSource.clip = vinylPlatesDic[(VynilPlateName)platenameInt].music;
+        audioSource.Play();
+    }
+
+    public void StopSong()
+    {
+        audioSource.Stop();
+    }
+
+
+
+}
